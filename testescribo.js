facbd41161f70486536080e2a32852c3927b061c
GabrielSales10 <99056850+GabrielSales10@users.noreply.github.com>
2022-08-20 23:02:56 -0300

    Update testescribo.js

--- testescribo.js
+++ testescribo.js
@@ -1,6 +1,6 @@
 function soma(number) {
     let soma= 0;
-    for(i = 2; i < number; i++) {
+    for(i = 0; i < number; i++) {
         if(i % 3 == 0 || i % 5 == 0) {
             soma += i;
         }
