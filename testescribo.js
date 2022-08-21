7ddcf7aed2aaf6335e82e34ef1075ee613f0a631
GabrielSales10 <99056850+GabrielSales10@users.noreply.github.com>
2022-08-20 22:53:45 -0300

    Add files via upload

---
+++ testescribo.js
@@ -0,0 +1,10 @@
+function soma(number) {
+    let soma= 0;
+    for(i = 2; i < number; i++) {
+        if(i % 3 == 0 || i % 5 == 0) {
+            soma += i;
+        }
+    }
+    return soma;
+}
+console.log(soma(12));
